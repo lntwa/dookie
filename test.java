98b4b5e0cac34ef8e442d11bf98ed6b238f71d27
arina <delonko@yandex.ru>
2024-08-12 23:25:45 +0500

    dookie

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+
+    public static void main(String[] args) {
+        
+        int age = 52;
+        System.out.println(age);
+
+        float pi = 3.14159f; //нужно указывать ф на конце :)
+        System.out.println(pi);
+
+        char ch = '%';
+        System.out.println(ch);
+
+    }
+}
